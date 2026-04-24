eb33f9216443a0ffc2c348ad08e67ceab5ee01a3
David Gabriel <davidgabrielsilvasouza43@gmail.com>
2026-04-23 21:22:21 -0300

    add \n nos prints

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    printf("Lucas Pinheiro Barbosa");
+    printf("Lucas Pinheiro Barbosa\n");
 
-    printf("Gustavo Araujo Ramos da Silva");
+    printf("Gustavo Araujo Ramos da Silva\n");
 
-    printf("David Gabriel Silva de Souza");
+    printf("David Gabriel Silva de Souza\n");
 
 
     return 0;
